ede1e9daaae1df95bdc7707a8c7a5b8478f28c8c
benravago <ben.ravago@aol.com>
2023-06-12 13:36:41 -0400

    Update ParsingExpressionGrammar.java

--- src/pp/grammar/peg/ParsingExpressionGrammar.java
+++ src/pp/grammar/peg/ParsingExpressionGrammar.java
@@ -1,23 +1,23 @@
 package pp.grammar.peg;
 
-import petit.parser.tools.GrammarDefinition;
-
-import static petit.parser.primitive.CharacterParser.whitespace;
-import static petit.parser.primitive.CharacterParser.pattern;
-import static petit.parser.primitive.CharacterParser.noneOf;
-import static petit.parser.primitive.CharacterParser.anyOf;
 import static petit.parser.primitive.CharacterParser.any;
+import static petit.parser.primitive.CharacterParser.anyOf;
 import static petit.parser.primitive.CharacterParser.of;
+import static petit.parser.primitive.CharacterParser.pattern;
+import static petit.parser.primitive.CharacterParser.whitespace;
 import static petit.parser.primitive.StringParser.of;
 
+import petit.parser.tools.GrammarDefinition;
+
 public class ParsingExpressionGrammar extends GrammarDefinition {
+
   public ParsingExpressionGrammar()  {
-    def("start", whitespace().star().seq(ref("grammar")) );
-    
+    def("start", whitespace().star().seq(ref("grammar")).seq(whitespace().star()) );
+
     /**
      *  from: A Text Pattern-Matching Tool based on Parsing Expression Grammars
      *    by: Roberto Ierusalimschy
-     * 
+     *
      *  grammar     <- ( nonterminal '<-' sp pattern )+
      *  pattern     <- alternative ( '/' sp alternative )*
      *  alternative <- ( [!&]? sp suffix )+
@@ -32,17 +32,25 @@ public class ParsingExpressionGrammar extends GrammarDefinition {
      *  nonterminal <- [a-zA-Z]+ sp
      *  sp          <- [ \t\n]*
      */
-    def("grammar", ref("nonterminal").seq(of("<-").trim()).seq(ref("pattern")).plus() );
-    def("pattern", ref("alternative").seq(of('/').trim().seq(ref("alternative")).star()) );
-    def("alternative", anyOf("!&").optional().trim().seq(ref("suffix")).plus() );
-    def("suffix", ref("primary").seq(anyOf("*+?").trim().star()) );
-    def("primary", of('(').trim().seq(ref("pattern")).seq(of(')')).trim()
-                   .or(of('.').trim())
-                   .or(ref("literal"))
-                   .or(ref("charclass"))
-                   .or(ref("nonterminal").seq(of("<-").not())) );
-    def("literal", of("'").seq(noneOf("'").star()).seq(of("'")).trim() );
-    def("charclass", of('[').seq(noneOf("]").seq(any().seq(of('-')).seq(any()).or(any()))).star().seq(of(']')).trim() );
+
+    def("grammar",     ref("nonterminal").seq(of("<-")).seq(ref("sp")).seq(ref("pattern")).plus() );
+    def("pattern",     ref("alternative").seq( of('/').seq(ref("sp")).seq(ref("alternative")).star() ) );
+    def("alternative", anyOf("!&").optional().seq(ref("sp")).seq(ref("suffix")).plus() );
+    def("suffix",      ref("primary").seq( anyOf("*+?").seq(ref("sp")).star() ) );
+
+    def("primary",     of('(').seq(ref("sp")).seq(ref("pattern")).seq(of(')')).seq(ref("sp"))
+                       .or(of('.').seq(ref("sp")))
+                       .or(ref("literal"))
+                       .or(ref("charclass"))
+                       .or(ref("nonterminal").seq(of("<-").not())) );
+
+    def("literal",     of("'").seq( of("'").not().seq( any() ).star() ).seq(of("'")).seq(ref("sp")) );
+    def("charclass",   of('[').seq( of(']').not().seq( any().seq(of('-')).seq(any() ).or(any()) ).star() ).seq(of(']')).seq(ref("sp")) );
+    def("nonterminal", pattern("a-zA-Z").plus().seq(ref("sp")) );
+    def("sp",          anyOf(" \t\n").star() );
+  }
+}
+
     def("nonterminal", pattern("a-zA-Z").plus().trim() );
     def("sp", anyOf(" \t\n").star() );
   }
